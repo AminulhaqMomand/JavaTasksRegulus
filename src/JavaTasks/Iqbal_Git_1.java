d7436eebd85806e11e68af52bfab880fd240f329
Iqbal <iqbalm.cydeo@gmail.com>
2023-05-25 13:59:38 -0600

    First file created

---
+++ src/JavaTasks/Iqbal_Git_1.java
@@ -0,0 +1,13 @@
+package JavaTasks;
+
+public class Iqbal_Git_1 {
+    public static void main(String[] args) {
+
+        System.out.println("I will welcome you all");
+        System.out.println("I hope all of our should be regular to the program to its logical end");
+
+
+
+    }
+
+}
