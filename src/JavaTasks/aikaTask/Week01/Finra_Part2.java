d030fbc128cd09764c88ee538f15ac7a5111d64b
Saim_Cydeo <at.crumrine@gmail.com>
2023-06-08 17:11:24 -0500

    Added Week01 plus added hand written task

---
+++ src/JavaTasks/aikaTask/Week01/Finra_Part2.java
@@ -0,0 +1,21 @@
+package JavaTasks.aikaTask.Week01;
+
+public class Finra_Part2 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 30; i++) {
+
+            if (i % 3 == 0 && i % 5 == 0){
+                System.out.print("FINRA; ");
+            }else if (i % 3 == 0){
+                System.out.print("FIN; ");
+            }else if (i % 5 == 0){
+                System.out.print("RA;  ");
+            }else {
+                System.out.print(i + " ");
+            }
+
+        }
+
+
+    }
+}
---
+++ src/JavaTasks/aikaTask/Week01/oddOrEven_Part2.java
@@ -0,0 +1,18 @@
+package JavaTasks.aikaTask.Week01;
+
+public class oddOrEven_Part2 {
+
+
+        public static void main(String[] args) {
+
+            OddOrEven(4);
+        }
+        public static void OddOrEven(int number){
+            if(number%2==0){
+                System.out.println("The number "+number+ " is Even");
+            }else{
+                System.out.println("The number "+number+ " is Odd");
+            }
+        }
+    }
+
