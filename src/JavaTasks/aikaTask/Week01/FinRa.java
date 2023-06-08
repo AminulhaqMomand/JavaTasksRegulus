fd7344d6f4b0324de74e87e2373f35f1d76624a7
Saim_Cydeo <at.crumrine@gmail.com>
2023-06-08 16:37:55 -0500

    Odd or Even and Finra

---
+++ src/JavaTasks/aikaTask/Week01/FinRa.java
@@ -0,0 +1,27 @@
+package JavaTasks.aikaTask.Week01;
+
+public class FinRa {
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 30; i++) {
+
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.print("FINRA; ");
+            } else if (i % 3 == 0) {
+                System.out.print("FIN; ");
+            } else if (i % 5 == 0) {
+                System.out.print("RA;  ");
+            } else {
+                System.out.print(i + " ");
+            }
+
+        }
+    }
+}
+
+
+
+
+
+
+
---
+++ src/JavaTasks/aikaTask/Week01/oddOrEven.java
@@ -0,0 +1,9 @@
+package JavaTasks.aikaTask.Week01;
+
+public class oddOrEven {
+    public static void main(String[] args) {
+
+
+
+    }
+}
