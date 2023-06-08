3ac1756855a9085401db4f64a7990b79c2e87461
Milena <milena86radojicic@gmail.com>
2023-06-08 16:37:13 -0500

    Added Week01 and added task 01 & 02

--- src/JavaTasks/MilenaTask/week1/Task01.java
+++ src/JavaTasks/MilenaTask/week1/Task01.java
@@ -1,12 +1,25 @@
 package JavaTasks.MilenaTask.week1;
 
+import java.util.Scanner;
+
 public class Task01 {
     public static void main(String[] args) {
 
+        int num = 5;
 
-
-
-
+        if (num % 2 == 0){
+            System.out.println(num + " is even number");
+        } else {
+            System.out.println(num + " is odd number");
+        }
 
     }
 }
+/*
+1. Write a method which can identifies given number is even or odd
+
+    EX:
+
+    identify(5) - > "Odd"
+    identify(6) - > "Even"
+ */
